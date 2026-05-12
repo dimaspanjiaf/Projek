5262d2d097cb8d136d1d7ad02b5a1f11810b3e17
fadhianszz <fadhilanshor24@gmail.com>
2026-05-12 13:50:20 +0700

    edit dashboard css js

--- script.js
+++ script.js
@@ -45,3 +45,140 @@ function login() {
         msg.innerText = 'Email atau password salah!';
     }
 }
+
+//================== DASHBOARD =================
+let dataTerhapus = null;
+let buku = JSON.parse(localStorage.getItem("buku")) || [
+    {
+        judul: "Laskar Pelangi",
+        penulis: "Andrea Hirata",
+        tahun: 2005,
+        kategori: "Novel"
+    },
+    {
+        judul: "Bumi",
+        penulis: "Tere Liye",
+        tahun: 2014,
+        kategori: "Fiksi"
+    },
+    {
+        judul: "Atomic Habits",
+        penulis: "James Clear",
+        tahun: 2018,
+        kategori: "Self Improvement"
+    }
+];
+
+const tbody = document.getElementById("data-buku");
+
+//TAMPILKAN DATA
+function tampilkanBuku() {
+
+    tbody.innerHTML = "";
+
+    buku.forEach((item, index) => {
+
+        const row = `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${item.judul}</td>
+                <td>${item.penulis}</td>
+                <td>${item.tahun}</td>
+                <td>
+                    <div class="aksi">
+
+                        <a href="detail-book.html?id=${index}" class="btn-detail">
+                        Detail
+                        </a>
+
+                        <button onclick="hapusBuku(${index})" class="btn-hapus">
+                        Hapus
+                        </button>
+
+                    </div>
+                </td>
+            </tr>
+        `;
+
+        tbody.innerHTML += row;
+    });
+
+    document.getElementById("total-buku").innerText = buku.length;
+
+    const kategoriUnik = [...new Set(buku.map(item => item.kategori))];
+
+    document.getElementById("total-kategori").innerText = kategoriUnik.length;
+
+    document.getElementById("total-user").innerText = 30;
+}
+
+//DETAIL BUKU
+function detailBuku(index) {
+
+    const item = buku[index];
+
+    alert(
+        "Judul: " + item.judul + "\n" +
+        "Penulis: " + item.penulis + "\n" +
+        "Tahun: " + item.tahun + "\n" +
+        "Kategori: " + item.kategori
+    );
+}
+
+//HAPUS BUKU
+function hapusBuku(index) {
+
+    let yakin = confirm("Yakin ingin menghapus buku?");
+
+    if (yakin) {
+
+        dataTerhapus = {
+            data: buku[index],
+            index: index
+        };
+
+        buku.splice(index, 1);
+
+        localStorage.setItem("buku", JSON.stringify(buku));
+
+        tampilkanBuku();
+
+        tampilkanUndo();
+    }
+}
+
+function undoHapus() {
+
+    if (dataTerhapus !== null) {
+
+        buku.splice(
+            dataTerhapus.index,
+            0,
+            dataTerhapus.data
+        );
+
+        localStorage.setItem("buku", JSON.stringify(buku));
+
+        tampilkanBuku();
+
+        document.getElementById("undo-box").innerHTML = "";
+
+        dataTerhapus = null;
+    }
+}
+
+function tampilkanUndo() {
+
+    const undoBox = document.getElementById("undo-box");
+
+    undoBox.innerHTML = `
+        <div class="undo-alert">
+            Buku berhasil dihapus
+
+            <button onclick="undoHapus()" class="btn-undo">
+                Undo
+            </button>
+        </div>
+    `;
+}
+tampilkanBuku();
